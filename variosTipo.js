73175a2fc183d021d3df79dd7fe459677620d810
Luis Felipe Aguilar Pereda <luis.aguilarpereda@gmail.com>
2017-10-20 12:25:17 -0500

    Update variosTipo.js

--- variosTipo.js
+++ variosTipo.js
@@ -1,176 +1,178 @@
-[
-  {
-    "tipo": "",
-    "valor": "",
-    "codigo": "",
-    "nombre": "Tipo Asistente",
-    "descripcion": "Tipo Asistente",
-    "fecha": "",
-    "datos": "",
-    "hijos": [
-      {
-        "nombre": "Parvulo",
-        "descripcion": "Parvulo",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "Principiante",
-        "descripcion": "Principiante",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "Primario",
-        "descripcion": "Primario",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "Intermedio",
-        "descripcion": "Intermedio",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "Jóvenes",
-        "descripcion": "Jóvenes",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "Adulto",
-        "descripcion": "Adulto",
-        "fecha": "",
-        "datos": ""
-      }
-    ]
-  },
-  {
-    "tipo": "",
-    "valor": "",
-    "codigo": "",
-    "nombre": "Etapas de Vida",
-    "descripcion": "Etapas de Vida",
-    "fecha": "",
-    "datos": "",
-    "hijos": [
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "PRENATAL",
-        "descripcion": "PRENATAL",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "INFANCIA",
-        "descripcion": "INFANCIA",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "NIÑEZ",
-        "descripcion": "NIÑEZ",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "ADOLESCENCIA",
-        "descripcion": "ADOLESCENCIA",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "JUVENTUD",
-        "descripcion": "JUVENTUD",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "ADULTEZ",
-        "descripcion": "ADULTEZ",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "ANCIENIDAD",
-        "descripcion": "ANCIENIDAD",
-        "fecha": "",
-        "datos": ""
-      }
-    ]
-  },
-  {
-    "tipo": "",
-    "valor": "",
-    "codigo": "",
-    "nombre": "Turnos diarios",
-    "descripcion": "Turnos diarios",
-    "fecha": "",
-    "datos": "",
-    "hijos": [
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "Mañana",
-        "descripcion": "Mañana",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "Tarde",
-        "descripcion": "Tarde",
-        "fecha": "",
-        "datos": ""
-      },
-      {
-        "tipo": "",
-        "valor": "",
-        "codigo": "",
-        "nombre": "Noche",
-        "descripcion": "Noche",
-        "fecha": "",
-        "datos": ""
-      }
-    ]
-  }
-]
+{
+  "data": [
+    {
+      "tipo": "",
+      "valor": "",
+      "codigo": "",
+      "nombre": "Tipo Asistente",
+      "descripcion": "Tipo Asistente",
+      "fecha": "",
+      "datos": "",
+      "hijos": [
+        {
+          "nombre": "Parvulo",
+          "descripcion": "Parvulo",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "Principiante",
+          "descripcion": "Principiante",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "Primario",
+          "descripcion": "Primario",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "Intermedio",
+          "descripcion": "Intermedio",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "Jóvenes",
+          "descripcion": "Jóvenes",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "Adulto",
+          "descripcion": "Adulto",
+          "fecha": "",
+          "datos": ""
+        }
+      ]
+    },
+    {
+      "tipo": "",
+      "valor": "",
+      "codigo": "",
+      "nombre": "Etapas de Vida",
+      "descripcion": "Etapas de Vida",
+      "fecha": "",
+      "datos": "",
+      "hijos": [
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "PRENATAL",
+          "descripcion": "PRENATAL",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "INFANCIA",
+          "descripcion": "INFANCIA",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "NIÑEZ",
+          "descripcion": "NIÑEZ",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "ADOLESCENCIA",
+          "descripcion": "ADOLESCENCIA",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "JUVENTUD",
+          "descripcion": "JUVENTUD",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "ADULTEZ",
+          "descripcion": "ADULTEZ",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "ANCIENIDAD",
+          "descripcion": "ANCIENIDAD",
+          "fecha": "",
+          "datos": ""
+        }
+      ]
+    },
+    {
+      "tipo": "",
+      "valor": "",
+      "codigo": "",
+      "nombre": "Turnos diarios",
+      "descripcion": "Turnos diarios",
+      "fecha": "",
+      "datos": "",
+      "hijos": [
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "Mañana",
+          "descripcion": "Mañana",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "Tarde",
+          "descripcion": "Tarde",
+          "fecha": "",
+          "datos": ""
+        },
+        {
+          "tipo": "",
+          "valor": "",
+          "codigo": "",
+          "nombre": "Noche",
+          "descripcion": "Noche",
+          "fecha": "",
+          "datos": ""
+        }
+      ]
+    }
+  ]
+}
